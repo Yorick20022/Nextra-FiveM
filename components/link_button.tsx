9ca258a2cf75bff325640a9f7017eee9012c9090
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2024-07-13 23:39:41 +0200

    :3

---
+++ components/link_button.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { blue } from "@mui/material/colors";
+
+interface LinkButtonProps {
+    text: string,
+    image: string
+}
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    boxShadow: 24,
+};
+
+
+const LinkButton: React.FC<LinkButtonProps> = ({ text, image }) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <code className={"nx-border-black nx-border-opacity-[0.04] nx-bg-opacity-[0.03] nx-bg-black nx-break-words nx-rounded-md nx-border nx-py-0.5 nx-px-[.25em] nx-text-[.9em] dark:nx-border-white/10 dark:nx-bg-white/10"}
+                onClick={handleOpen}
+                style={{ cursor: "pointer", color: blue[500], textDecoration: "underline" }}
+            >{text}
+            </code>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...style, width: 500 }}>
+                    <img src={image} alt={text} style={{ width: "100%", height: "100%" }} />
+                </Box>
+            </Modal>
+        </>
+    );
+}
+
+export default LinkButton;
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/yorick20022',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://www.youtube.com/watch?v=xvFZjo5PgG0',
   footer: {
     text: 'Nextra Docs Template',
   },
